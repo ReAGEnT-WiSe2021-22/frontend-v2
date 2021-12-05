6b8c10bff0c5f6686164935276ef186ac18a43ed
Nathanael H <nathanael887@gmail.com>
2021-12-06 00:04:06 +0100

    feat(search-engine): created searchbar and filters

--- packages/search-engine/src/direflow-components/search-engine/App.tsx
+++ packages/search-engine/src/direflow-components/search-engine/App.tsx
@@ -1,33 +1,124 @@
-import { Styled } from 'direflow-component';
-import React from 'react';
-import { useFetch } from '../../hooks/use-fetch';
+import { Styled } from 'direflow-component'
 import styles from './App.css';
+import React, { FC } from 'react';
+import { ReactiveBase, DataSearch, DateRange, MultiDataList } from '@appbaseio/reactivesearch';
 
-export interface Tweet {
-  id: number
-  text: string
-}
+const partyNames = [
+  {
+      label: 'CDU',
+      value: 'CDU',
+  },
+  {
+      label: 'AFD',
+      value: 'AFD',
+  },
+  {
+      label: 'Grüne',
+      value: 'Grüne',
+  },
+  {
+    label: 'Linke',
+    value: 'Linke',
+  },
+]
 
-const App: React.FunctionComponent = () => {
-  const [tweets, setTweets] = React.useState<Tweet[]>();
-  const { fetchApi } = useFetch();
-
-  React.useEffect(() => {
-    fetchApi('api/search-tweets')
-      .then((res) => res.json())
-      .then((json) => setTweets(json.tweets));
-  }, [fetchApi]);
-
+const App: FC = () => {
   return (
     <Styled styles={styles}>
-      <div className="app">
-        {tweets && tweets.map((tweet) => (
-          <div key={tweet.id}>
-            <span style={{ marginRight: '1rem' }}>{tweet.id}</span>
-            <span>{tweet.text}</span>
-          </div>
-        ))}
+    <ReactiveBase
+          app="search-engine"
+          url="http://twint.f4.htw-berlin.de:9200/"
+          enableAppbase
+          
+
+          theme={{
+            typography: {
+               fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Noto Sans", "Ubuntu", "Droid Sans", "Helvetica Neue", sans-serif',fontSize: "16px"
+            },
+            colors: {
+                  textColor: "#fff",
+                  backgroundColor: "#212121",
+                  primaryTextColor: "#fff",
+                  primaryColor: "#2196F3",
+                  titleColor: "#fff",
+                  alertColor: "#d9534f",
+                  borderColor: "#666"
+            }
+          }}
+    >
+      
+      <div className="navbar">
+        
+        <div className="logo-container">
+          
+        </div>
+         
+        
+
+        <div className="search-container">
+          <DataSearch            
+            componentId="mainSearch"            
+            dataField={["party","party.search", "tweets", "tweets.search"]}                      
+            className="search-bar"            
+            queryFormat="and"            
+            placeholder="Search here..."
+            innerClass={{
+              "input": "searchbox",
+              "list": "suggestionlist"
+            }}                  
+          />
+        </div>
       </div>
+      
+      
+      {/*Filters*/}
+      <div className="left-bar">
+        <div>
+          <div className="filter-heading center">
+            <b>
+              {" "}
+              <i className="fa fa-calendar" /> Party Filter{" "}
+            </b>
+          </div> 
+
+          <MultiDataList
+              componentId="party-list"
+              dataField="party"
+              data={partyNames}
+             
+              queryFormat="or"
+              selectAllLabel="All Party"
+              showCheckbox={true}
+              showSearch={true}
+              placeholder="Search for a party"
+              react={{
+                and: [
+                  "mainSearch",
+                  "results",
+                  "date-filter",
+                ]
+              }}
+    
+          />
+
+            
+          <hr className="blue" />
+
+          <div className="filter-heading center">
+            <b>
+              {" "}
+              <i className="fa fa-calendar" /> Tweet Date{" "}
+            </b>
+          </div>
+          <DateRange
+            componentId="date-filter"
+            dataField="Tweet_date"
+            className="datePicker"
+          />
+        </div>
+      </div>
+    
+    </ReactiveBase>
     </Styled>
   );
 };

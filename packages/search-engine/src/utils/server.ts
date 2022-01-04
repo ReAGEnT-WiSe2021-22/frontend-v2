53e90340d784461999ffdd54c3de9a871705a0d4
Nathanael Hermanto <nathanael887@gmail.com>
2022-01-04 14:21:25 +0100

    fix(search-engine): deleted server.ts

--- packages/search-engine/src/utils/server.ts
+++
@@ -1,9 +0,0 @@
-import { createServer } from 'miragejs';
-
-export const createMockServer = () => {
-  (window as any).server = createServer({
-    routes() {
-      this.passthrough()
-    },
-  });
-};

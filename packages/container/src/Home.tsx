85754a9b4f743f311eba3ff027dd8338504b6a01
sunanmau5 <sunan.regi@gmail.com>
2022-01-11 20:54:06 +0100

    refactor: small styling changes

--- packages/container/src/Home.tsx
+++ packages/container/src/Home.tsx
@@ -3,7 +3,7 @@ import ReactMarkdown from 'react-markdown';
 import './Home.css';
 
 const Home = ({ wiki }: {wiki: string}): JSX.Element => (
-  <div className="home">
+  <div className="home p-12">
     <ReactMarkdown>
       {wiki}
     </ReactMarkdown>
--- packages/potential-party/src/direflow-components/potential-party/components/tweet-details.tsx
+++ packages/potential-party/src/direflow-components/potential-party/components/tweet-details.tsx
@@ -1,11 +1,38 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import React from 'react';
 import { Tweet } from '../../../types';
 
-export const TweetDetails: React.FC<{ tweet?: Tweet }> = ({ tweet }) => (
-  <Box>
-    {tweet
-      ? <Typography variant="caption"><pre>{JSON.stringify(tweet, null, 2)}</pre></Typography>
-      : <Typography>No tweets dropped</Typography>}
-  </Box>
-);
+export const TweetDetails: React.FunctionComponent<{
+  tweet?: Tweet
+  onClear: () => void
+}> = ({ tweet, onClear }) => {
+  const alignment = tweet ? 'start' : 'center';
+
+  let content: React.ReactElement;
+  if (!tweet) {
+    content = <Typography>No tweets dropped</Typography>;
+  } else {
+    content = (
+      <Box>
+        <Typography variant="caption">
+          <pre>{JSON.stringify(tweet, null, 2)}</pre>
+        </Typography>
+        <Button onClick={onClear}>Clear</Button>
+      </Box>
+    );
+  }
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: alignment,
+        alignItems: alignment,
+        minWidth: 120,
+      }}
+      height={500}
+    >
+      {content}
+    </Box>
+  );
+};

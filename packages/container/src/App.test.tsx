4bfcfc31be65b3ebdd350d3aae377df5cdedb8fb
Louis Andrew <louisandrew3@gmail.com>
2022-01-11 11:38:44 +0100

    test(container): mock react-markdown on test

--- packages/container/src/App.test.tsx
+++ packages/container/src/App.test.tsx
@@ -3,10 +3,17 @@ import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
+jest.mock(
+  'react-markdown',
+  () => function (props: { children: any }) {
+    return props.children;
+  },
+);
+
 test('renders frontend v2 title', () => {
   render(
     <MemoryRouter>
-      <App />
+      <App wiki="" />
     </MemoryRouter>,
   );
   const linkElement = screen.getByText(/Home/i);

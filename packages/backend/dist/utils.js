020d4ee398e67a4ec36b79178ff7698b08916f9e
Louis Andrew <louisandrew3@gmail.com>
2022-01-26 16:37:24 +0100

    Add build files v2

---
+++ packages/backend/dist/utils.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.manipulatePartyReputationUpstream = void 0;
+// eslint-disable-next-line import/prefer-default-export
+const manipulatePartyReputationUpstream = (data) => {
+    const [rawData, mlData] = data;
+    return mlData.map((partyReputationData) => {
+        var _a;
+        const { party } = partyReputationData;
+        return Object.assign(Object.assign({}, partyReputationData), { rawData: ((_a = rawData.find((raw) => raw.party === party)) === null || _a === void 0 ? void 0 : _a.values) || [] });
+    });
+};
+exports.manipulatePartyReputationUpstream = manipulatePartyReputationUpstream;
+//# sourceMappingURL=utils.js.map

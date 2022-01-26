c44d655b909d8add378cefc0c22d794b6abbd8c9
Louis Andrew <louisandrew3@gmail.com>
2022-01-26 10:56:30 +0100

    feat(backend): add controller to fetch and manipulate upstream party-rep data

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,11 +1,33 @@
+/* eslint-disable import/extensions */
 import express from 'express';
 import { join } from 'path';
+import axios from 'axios';
+// eslint-disable-next-line import/no-unresolved
+import { PartyReputationUpstreamType } from './types';
+// eslint-disable-next-line import/no-unresolved
+import { manipulatePartyReputationUpstream } from './utils';
 
 const app = express();
 const port = 8080;
 
 const v2Path = join(__dirname, '../frontends');
 app.use(express.static(v2Path));
+
+app.get('/api/party-reputation', async (_, res) => {
+  const externalUrl = 'http://reagent1.f4.htw-berlin.de:8123';
+
+  try {
+    const upstreamRequest = await axios.get(externalUrl);
+    const upstreamData: PartyReputationUpstreamType = upstreamRequest.data;
+    const manipulatedData = manipulatePartyReputationUpstream(upstreamData);
+
+    res.send({ msg: 'success', data: manipulatedData });
+
+    return;
+  } catch (e) {
+    res.status(400).send({ msg: 'Bad request', e });
+  }
+});
 
 app.get('/v1', (req, res) => {
   res.sendFile(join(__dirname, '../frontends', 'v1/index.html'));
---
+++ packages/backend/src/types.ts
@@ -0,0 +1,7 @@
+export type PartyReputationType = {
+ party: string,
+ dates: string[]
+ values: number[]
+}[]
+
+export type PartyReputationUpstreamType = [PartyReputationType, PartyReputationType]
